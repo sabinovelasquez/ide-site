703f2c00aa51f8a1d1b8226d509a450c00c450c8
Sabino Velasquez <depointless@Sabinos-Mac-mini.local>
2015-08-12 01:48:36 -0300

    initial settings, gemfile, rakefile

--- config.rb
+++ config.rb
@@ -1,32 +1,41 @@
+# config.rb
+require 'uglifier'
+
 set :css_dir, 'css'
 set :js_dir, 'js'
 set :images_dir, 'img'
+set :partials_dir, 'partial'
 set :haml, { :ugly => true, :format => :html5 }
+activate :minify_html
+activate :livereload
+activate :asset_hash
+activate :cache_buster
 
-configure :build do
-  activate :sprockets
-  activate :bootstrap
-  activate :minify_css
-  activate :minify_javascript
-  activate :minify_html
-  activate :relative_assets
+configure :development do
+  set :debug_assets, true
 end
 
+configure :build do
+  activate :minify_javascript
+  activate :minify_css
+  activate :relative_assets
+end
 
 case ENV['TARGET'].to_s.downcase
   when 'production'
     activate :deploy do |deploy|
-    deploy.method   = :ftp
-    deploy.host     = 'public_thml'
-    deploy.path     = '/'
-    deploy.user     = 'ide'
-    deploy.password = 'pass'
+    deploy.method   = :sftp
+    deploy.port     = 22
+    deploy.host     = '45.55.240.255'
+    deploy.path     = '/var/www/inglesdondestes.cl/public_html/'
+    deploy.user     = 'sabino'
     deploy.build_before = true
     end
-  else ## Otro Ambiente
+  else
     activate :deploy do |deploy|
-        deploy.method = :git
+      deploy.method = :git
       deploy.branch   = 'bb-middleman'
       deploy.commit_message = 'Commit - middleman branch'
     end
   end
+
